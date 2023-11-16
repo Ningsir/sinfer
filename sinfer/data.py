76b3ad2e86bbd08515316cec3b4c4ced9ab6fe4c
Ningsir <1819397331@qq.com>
2023-11-16 19:56:37 +0800

    support pyg data

--- sinfer/data.py
+++ sinfer/data.py
@@ -4,17 +4,19 @@ import torch as th
 import numpy as np
 
 
-class SinferDataset():
+class SinferDataset:
     def __init__(self, path):
-        self.coo_path = os.path.join(path, 'coo.bin')
-        self.features_path = os.path.join(path, 'feat.bin')
-        self.offsets_path = os.path.join(path, 'offsets.txt')
-        self.conf_path = os.path.join(path, 'conf.json')
-         
-        self.conf = json.load(open(self.conf_path, 'r'))
-        self._num_nodes = self.conf['num_nodes']
-        self._feat_dim = self.conf['feat_dim']
-        self._num_classes = self.conf['num_classes']
+        self.coo_path = os.path.join(path, "coo.bin")
+        self.features_path = os.path.join(path, "feat.bin")
+        self.offsets_path = os.path.join(path, "offsets.txt")
+        self.conf_path = os.path.join(path, "conf.json")
+        self.indptr_path = os.path.join(path, "indptr.bin")
+        self.indices_path = os.path.join(path, "indices.bin")
+
+        self.conf = json.load(open(self.conf_path, "r"))
+        self._num_nodes = self.conf["num_nodes"]
+        self._feat_dim = self.conf["feat_dim"]
+        self._num_classes = self.conf["num_classes"]
 
     def coo(self):
         coo = np.fromfile(self.coo_path, dtype=np.int64)
@@ -22,28 +24,41 @@ class SinferDataset():
         coo_row = th.from_numpy(coo[0])
         coo_col = th.from_numpy(coo[1])
         return coo_row, coo_col
-    
+
     @property
     def feat_path(self):
         return self.features_path
-    
+
     @property
     def num_nodes(self):
         return self._num_nodes
-    
+
     @property
     def feat_dim(self):
         return self._feat_dim
-    
+
     @property
     def num_classes(self):
         return self._num_classes
 
     @property
     def offsets(self):
-        offsets = np.fromfile(self.offsets_path, sep='\n', dtype=np.int64)
+        offsets = np.fromfile(self.offsets_path, sep="\n", dtype=np.int64)
         return list(offsets)
 
+    @property
+    def indptr(self):
+        indptr_ = np.fromfile(self.indptr_path, dtype=np.int64)
+        indptr_ = th.from_numpy(indptr_)
+        return indptr_
+
+    @property
+    def indices(self):
+        indices_ = np.fromfile(self.indices_path, dtype=np.int64)
+        indices_ = th.from_numpy(indices_)
+        return indices_
 
     def __str__(self) -> str:
-        return "num_nodes: {}, feat_dim: {}, num_classes: {}".format(self.num_nodes, self.feat_dim, self.num_classes)
+        return "num_nodes: {}, feat_dim: {}, num_classes: {}".format(
+            self.num_nodes, self.feat_dim, self.num_classes
+        )
